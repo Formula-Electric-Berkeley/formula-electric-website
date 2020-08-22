90bbc177f4fc7a3fc2afa1ac9d1abc3d0ec161b0
Ethan Wu <ewu000@berkeley.edu>
2020-08-22 03:17:14 -0500

    done

--- src/components/Join.js
+++ src/components/Join.js
@@ -16,7 +16,12 @@ const Join = () => {
 				<h4>
 				<br/>
 				We are looking for mechanical, electrical, and business teammates.<br/><br/>
-				Join our infosession to learn about FEB and the recruitment process and chat with our team.<br/>
+				Join our infosession to learn about FEB and the recruitment process and chat with our team.<br/><br/>
+				Two sessions:<br/>
+				1. Mon 8/31 8PM-9PM<br/>
+				2. Thu 9/3 8PM-9PM<br/><br/>
+				Zoom link: https://berkeley.zoom.us/j/93585932979<br/>
+				"Note: you must be logged in with your UC Berkeley Zoom account to enter this meeting."
 				</h4>
 			</div>
 		</div>
