d4d89f89d6e9de8696dd93f9f3f81dae3f7e7ee2
Eric Xu <ericxu2024@berkeley.edu>
2023-02-23 18:16:08 -0800

    created Big Give page to show up when website is visited

--- src/components/BigGive.js
+++ src/components/BigGive.js
@@ -5,15 +5,37 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import '../css/BigGive.css';
-
+import Button from 'react-bootstrap/Button';
 
 
 const BigGive = () => {
     return (
-        <div>
-            <p>hiwdahdiwhdd</p>
+        <div className="big-give">
+                <div className="center-horz">
+                    <a href="https://give.berkeley.edu/giftdetails?fund1=FU1349000">
+                        <Button className="button join-button" style={style.BigButton}>
+                            <p className="button-text">DONATE</p>
+                        </Button>
+                    </a>
+                </div>
+                <br />
+                <dahawkdwad></dahawkdwad>
+                <div className="center-horz">
+                    <a href="/landing">
+                        <Button className="button join-button">
+                            <p className="button-text">Homepage</p>
+                        </Button>
+                    </a>
+                </div>
         </div>
+        
     )
+}
+
+const style = {
+    BigButton: {
+        fontSize: "50px",
+    }
 }
 
 export default BigGive;
