4bf2ff2099e2e4e9576d4daf303e4bf49d904e3e
Eric Xu <ericxu2024@berkeley.edu>
2023-05-17 13:27:31 -0700

    updated mobile sponsors page

--- src/components/Sponsors.js
+++ src/components/Sponsors.js
@@ -9,7 +9,7 @@ import Col from 'react-bootstrap/Col';
 import HeaderSponsors from '../img/header-sponsors.png';
 import HeaderSponsorsMobile from '../img/mobile/header-sponsors-mobile.png';
 import SponsorPhotos from '../img/sponsoropaque.png';
-import SponsorPhotosMobile from '../img/mobile/Group 36.png';
+import SponsorPhotosMobile from '../img/mobile/sponsors-mobile-051723.png';
 
 import '../css/Sponsors.css';
 
